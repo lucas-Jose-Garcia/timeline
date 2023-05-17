5587c7eb5e4429989a1da798222526c3aeb1f201
lucas-Jose-Garcia <lucasjosegarcia01@gmail.com>
2023-05-17 19:00:24 -0300

    (A2) Ajustes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -50,10 +50,10 @@ export default function Home() {
         </div>
 
         {/** Copyright */}
-        <div className="text-sm leading-relaxed text-green-200">
+        <div className="text-sm leading-relaxed text-gray-200">
           Feito com 💜 no NLW da{' '}
           <a
-            href="#"
+            href="https://rocketseat.com.br"
             target="_blank"
             rel="noreferrer"
             className="underline hover:text-gray-100"
